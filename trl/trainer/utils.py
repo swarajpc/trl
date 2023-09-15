4a797682d25a669cc254b331c0cba67c2b0588f4
swarajpc <89372837+swarajpc@users.noreply.github.com>
2023-09-15 13:34:21 +0530

    Update utils.py
    Handling token length issue

--- trl/trainer/utils.py
+++ trl/trainer/utils.py
@@ -553,10 +553,10 @@ class ConstantLengthDataset(IterableDataset):
                     else:
                         more_examples = False
                         break
-            tokenized_inputs = self.tokenizer(buffer, truncation=False)["input_ids"]
+            tokenized_inputs = self.tokenizer(buffer, truncation=True, padding="max_length", max_length = self.seq_length)["input_ids"]
             all_token_ids = []
             for tokenized_input in tokenized_inputs:
-                all_token_ids.extend(tokenized_input + [self.concat_token_id])
+                all_token_ids.extend(tokenized_input)
             examples = []
             for i in range(0, len(all_token_ids), self.seq_length):
                 input_ids = all_token_ids[i : i + self.seq_length]
